bb0e45bcedd83ef5e661427fbe731f5e5142a8cc
RA-Valdez <valdezrinieladrian@gmail.com>
2023-12-04 17:16:42 +0800

    added domain setting for login cookie api

--- gacha-gears-server/routes/api/user.js
+++ gacha-gears-server/routes/api/user.js
@@ -60,8 +60,8 @@ router.post('/login', asyncHandler(async (req, res, next) => {
           .setAudience(process.env.API_AUDIENCE)
           .setExpirationTime(process.env.API_EXPIRE_TIME)
           .sign(new TextEncoder().encode(process.env.API_KEY));
-        res.cookie('token', jwt, { httpOnly: true, maxAge: 7200000, sameSite: 'none', secure: true });
-        res.cookie('public-token', user[0].username, { maxAge: 7200000, sameSite: 'none', secure: true });
+        res.cookie('token', jwt, { httpOnly: true, maxAge: 7200000, domain:process.env.CLIENT_DOMAIN, sameSite: 'none', secure: true });
+        res.cookie('public-token', user[0].username, { maxAge: 7200000, domain:process.env.CLIENT_DOMAIN, sameSite: 'none', secure: true });
         res.send("token sent");
       }
       else {
